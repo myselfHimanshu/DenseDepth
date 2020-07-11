ce79e13d7015cb2bcb619d94429bfcbcc2ec39e9
Himanshu <047himanshu@gmail.com>
2020-07-11 16:47:40 +0530

    model loading

--- test.py
+++ test.py
@@ -7,13 +7,14 @@ from keras.models import load_model
 from layers import BilinearUpSampling2D
 from utils import predict_
 
+# Custom object needed for inference and training
+custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
+
 # Load model into GPU / CPU
+model_path = "./DenseDepth/nyu.h5"
 model = load_model(model_path, custom_objects=custom_objects, compile=False)
 
-def return_output(model_path, images_list):
-    # Custom object needed for inference and training
-    custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
-
+def return_output(model_path, images_list):    
     # Input images
     inputs = load_images(images_list)
     print('\nLoaded ({0}) images of size {1}.'.format(inputs.shape[0], inputs.shape[1:]))
