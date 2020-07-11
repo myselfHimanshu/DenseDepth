d58977a50a5c4528cffa16cededb966115505159
Himanshu <047himanshu@gmail.com>
2020-07-11 16:43:19 +0530

    model loading

--- test.py
+++ test.py
@@ -7,16 +7,12 @@ from keras.models import load_model
 from layers import BilinearUpSampling2D
 from utils import predict_
 
+# Load model into GPU / CPU
+model = load_model(model_path, custom_objects=custom_objects, compile=False)
+
 def return_output(model_path, images_list):
     # Custom object needed for inference and training
     custom_objects = {'BilinearUpSampling2D': BilinearUpSampling2D, 'depth_loss_function': None}
-
-    print('Loading model...')
-
-    # Load model into GPU / CPU
-    model = load_model(model_path, custom_objects=custom_objects, compile=False)
-
-    print('\nModel loaded ({0}).'.format(model_path))
 
     # Input images
     inputs = load_images(images_list)
